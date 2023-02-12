a727b5a88eb4562b45c538225362d5de077def61
AliHussein <alihusseiinali@gmail.com>
2023-02-12 02:05:06 +0200

    transfere logic to the model

--- online-shop/app/Http/Controllers/DetailController.php
+++ online-shop/app/Http/Controllers/DetailController.php
@@ -12,30 +12,18 @@ class DetailController extends Controller
     //
     function index($id=9)
     {
-        $product = Product::findOrFail($id);
-        $products = Product::all();
-        $reviews = $reviews = Review::where('product_id', $id)->with('user')->get();
+        $product = Review::getProduct($id);
+        $products = Review::getProducts();
+        $reviews = Review::getReviews($id);
         
         
         return view('details')->with(['products'=>$products,'product'=>$product,'reviews'=>$reviews]);
     }
     function leaveAreview(Request $request, $id=9)
     {
-        $review = new Review;
-        $review['user_id'] = Auth::user()->id;
-        $review['product_id'] = $id;
-        $review['rating'] = 2.5;
-        $review->fill($request->post());
-        $review->save();
+        Review::postReview($request, $id=9);
         
-        $productReview = Review::where('product_id', '=', $id)->get();
-        $productTotalRating = $productReview->sum('rating');
-        $product = Product::findOrFail($id);
-        $product['rating_count'] = $product['rating_count']+1 ;
-        $rating_count = $product['rating_count'];
-        $product['rating'] =floor(($productTotalRating +$review['rating']*$rating_count)/$rating_count);
-         // product->rating = 4*
-        $product->save();
+       
         
         return Redirect()->back()->with('done', 'Done:Thank you for your Review');
     }
--- online-shop/app/Models/Review.php
+++ online-shop/app/Models/Review.php
@@ -4,10 +4,56 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Review extends Model
 {
     use HasFactory;
+
+    public static function getProduct($id=9)
+    {
+        $product = Product::findOrFail($id);
+        return $product; 
+    }
+    public static function getProducts()
+    {
+        $products = Product::all();
+        return $products; 
+    }
+      public static function getReviews($id=9)
+    {
+        $reviews = $reviews = Self::where('product_id', $id)->with('user')->get();
+        return $reviews;
+    }
+
+      public static function postReview($request, $id=9)
+    {
+        $review = new Self;
+        $review['user_id'] = Auth::user()->id;
+        $review['product_id'] = $id;
+        $review['rating'] = 3.5;
+        $review->fill($request->post());
+        $review->save();
+         $productReview = Self::where('product_id', '=', $id)->get();
+        $productTotalRating = $productReview->sum('rating');
+        $product = Product::findOrFail($id);
+        $product['rating_count'] = $product['rating_count']+1 ;
+        $rating_count = $product['rating_count'];
+        $product['rating'] =floor(($productTotalRating +$review['rating']*$rating_count)/$rating_count);
+         // product->rating = 4*
+        $product->save();
+
+    }
+   
+   
+
+
+
+
+
+
+
+    
     protected $guarded = [];
     function product()
     {
